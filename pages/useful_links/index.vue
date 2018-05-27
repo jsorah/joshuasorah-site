c01c6b8a40a987e2901a67f74a1a57b119692b85
Joshua Sorah <jsorah@gmail.com>
2018-05-26 23:15:54 -0400

    Initial phase of templating

--- pages/useful_links/index.vue
+++ pages/useful_links/index.vue
@@ -1,6 +1,48 @@
 <template lang="pug">
-    .hero.is-info    
-        .hero-body
-            h1.title.is-1 Useful Links
+    div
+        .hero.is-info
+            .hero-body
+                h1.title.is-1 Useful Links
+
+        .container(v-for="linkCategory in linkCategories")
+            section.section
+                .card            
+                    header.card-header
+                        h1.card-header-title {{linkCategory.name}}
+                    .card-content
+                        ul
+                            li(v-for="link in linkCategory.links")                            
+                                a(:href="link.url") {{link.name}}                                
+                                    //- small {{link.description}}
 </template>
 
+
+
+<script>
+export default {
+    asyncData ( context) {
+        return { 
+            linkCategories: [
+                {
+                    name: "Security",
+                    links: [
+                        { 
+                                url: "https://krebsonsecurity.com/",
+                                name: "Krebs on Security",
+                                description: "Brian Krebs Blog"
+                        },
+                        { 
+                                url: "https://www.hackthebox.eu/",
+                                name: "Hack The Box - Penetration Labs / Challenges",
+                                description: ""
+                        }
+                    ]
+                },
+                {
+                    name: "Place Holder",
+                }
+            ]
+        }
+    }
+}
+</script>
